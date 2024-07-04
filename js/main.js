4761d721e497e4d8a15570ef9b135d6b1bff1ac1
shakhzoda05 <shakhzoda05@gmail.com>
2024-07-05 04:55:45 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 let elLIst = document.querySelector(".pokemon-list");
-
+let elSelect=document.querySelector(".pokemon-select");
+let elInput=document.querySelector(".pokemon-input")
 function renderPOkemon(arr, list) {
-  arr.forEach((item) => {
+  list.innerHTML=""
+  arr.forEach(item => {
     let elItem = document.createElement("li");
     elItem.innerHTML = `
             <img src="${item.img}" alt="${item.name}"> 
-            <div class="flex items-center justify-between">
+            <div class="flex items-center justify-between gap-[5px]">
                 <div>
                     <h2>${item.name}</h2>
-                    <p>Type:${item.type[0]}</p>
+                    <p class="flex"> <span></span>Type:</span>${item.type.join(",")}</p>
                 </div>
+                
                 <div>
                     <div class="flex items-center gap-[10px]">
                     <button class="p-[5px] rounded-[5px] border-[2px] border-red-500 text-red-500">Like</button>
-                    <button class="p-[5px] rounded-[5px] border-[2px] border-green-500 text-green-500">More</button>
+                    <button class="p-[5px] rounded-[5px] border-[2px] border-green-500 text-green -500">More</button>
                 </div>
             </div> `;
     elItem.className =
@@ -21,4 +24,45 @@ function renderPOkemon(arr, list) {
     list.append(elItem);
   });
 }
-renderPOkemon(pokemons.splice(0, 20), elLIst);
+function renderSelectOption(arr, list) {
+  let listOption = [];
+  let sortedOptionContent = [];
+  arr.forEach(item => {
+    listOption = listOption.concat(item.type);
+  });
+  listOption.filter(item => {
+    if (!sortedOptionContent.includes(item)) {
+      sortedOptionContent.push(item);
+    }
+  });
+  console.log(listOption);
+  console.log(sortedOptionContent);
+
+  sortedOptionContent.map(item => {
+    let elOption = document.createElement("option");
+    elOption.value = item;
+    elOption.textContent = item;
+    list.appendChild(elOption);
+  });
+}
+
+
+
+renderSelectOption(pokemons, elSelect);
+renderPOkemon(pokemons, elLIst);
+
+elInput.addEventListener("keyup", (e) =>{
+  const inputValue=e.target.value.toLowerCase();
+  const searchData=pokemons.filter(item=>item.name.toLowerCase().includes(inputValue))
+renderPOkemon(searchData,elLIst)
+})
+
+elSelect.addEventListener("change", function(e){
+  const selectValue=e.target.value
+  const selectedPokemons=pokemons.filter(item=>item.type.includes(selectValue))
+  console.log()
+  renderPOkemon(selectedPokemons,elLIst)
+})
+
+
+
